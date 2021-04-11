e9ab92e4fd8a4663b31ace8ea6530c488c0049e6
RizkyRamadhan14 <ramadhanrrrizky@gmail.com>
2021-04-11 09:10:00 +0700

    4/11 9AM

--- app/src/main/java/com/example/tugas3/MainActivity.java
+++ app/src/main/java/com/example/tugas3/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.tugas3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton Logo;
+    Button Submit;
+    EditText InputNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Logo = findViewById(R.id.IB1);
+        Submit = findViewById(R.id.B1);
+        InputNama = findViewById(R.id.ET1);
+
+        Logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent website = new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.formula1.com/"));
+                startActivity(website);
+            }
+        });
+
+        Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nama = new Intent(MainActivity.this,TeamList.class);
+                nama.putExtra("nama",InputNama.getText().toString());
+                startActivity(nama);
+            }
+        });
     }
 }
